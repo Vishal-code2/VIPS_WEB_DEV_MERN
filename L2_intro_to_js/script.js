285c598267eae73e77ab82107cf7ab62b7440635
Vishal <professional96gd@gmail.com>
2025-07-11 11:13:24 +0530

    Add initial HTML, JavaScript, and CSS files for Day 2 project

---
+++ L2_intro_to_js/script.js
@@ -0,0 +1,187 @@
+// console.log("Hello ");
+
+
+
+// function sumOfTwo(a,b){
+//     return a+b;
+// }
+// function sumOfTwo(a,b){ //create two ec
+//     return a+b;
+// }
+// var a =200;
+// var b =500;
+// console.log(sumOfTwo(a,b));
+
+
+// function firstFun(){
+//     var a =20;
+//     var b =30;
+//     console.log("HELLO I AM FIRST FUNCTION");
+
+//     function secFun(){
+//         var c = 50;
+//         console.log("HELLO I AM SECOND FUNCTION");
+//         function thirdFun(){
+//             var d =80;
+//             console.log("HELLO I AM THIRD FUNCTION");
+//             console.log(a+b+c+d);
+//         }
+//         thirdFun();
+//     }
+//     secFun();
+// }
+// console.log(firstFun());
+
+
+// for (var i =0; i<=4; i++){        //wrong code 
+
+
+// setTimeout(function(){
+//         console.log(i);
+//     },2000);
+// }
+
+
+// hello()
+// function hello(){
+//     console.log("hello");
+// }
+
+
+
+// var count=0;
+
+//     var intervalID = setInterval(function(){
+//         console.log(count);
+//         count++;
+//         if(count==5){
+//     clearInterval(intervalID);
+//     }
+// },2000);
+
+
+// for(var a=0; a<=5; a++){
+//     function close(i){
+        
+// setTimeout(function(a){
+//         console.log(i);
+//     },2000);
+// }
+
+//     close(a);
+// }
+
+
+// console.log("start");
+// var a=10;
+// console.log(a);
+// var functionB = function(){
+//     console.log("hello ")
+// }
+// functionB();
+
+// var functionB = function (){
+//     console.log("hello world")
+// }
+// functionB();
+
+
+// function xyz(ag1,ag2){
+//     console.log("add");
+//     console.log("a=20 and b=30");
+
+//     var c=a+b;
+//     return c;
+// }
+
+// var a=20;
+// var b=30;
+// console.log(xyz(a,b));
+
+
+// function function_y(){
+//     console.log(" vips");
+
+// }
+
+// function_y(functionx);
+
+// function c(){
+//     console.log("hello");
+//     function d(){
+//         console.log("vips");
+
+//     }
+//     return d();
+// }
+
+// c();
+
+
+
+
+// my code
+// for (i=0;i<=4;i++){
+//     if (i==0){
+// setTimeout(() => {
+//     console.log("Menu");
+// }, 5000)
+//     }
+//     if (i==1){
+// setTimeout(() => {
+//     console.log("Order");
+// }, 2000)
+//     }
+//     if(i==2){
+// setTimeout(() => {
+//     console.log("Dinner");
+// }, 10000)
+//     }
+
+// if(i==3){
+// setTimeout(() => {
+//     console.log("Sauf");
+
+// }, 1000)
+// }
+// if(i==4){
+// setTimeout(() => {
+
+//     console.log("Pay");
+// }, 3000);
+// }}
+
+
+// function a() {setTimeout(() => {
+//     console.log("Menu");
+// }, 5000)
+
+//     function b(){setTimeout(() => {
+//     console.log("Order");
+// }, 2000)
+
+//             function c(){setTimeout(() => {
+//     console.log("Dinner");
+// }, 10000)
+
+
+// function d(){setTimeout(() => {
+//     console.log("Sauf");
+
+// }, 1000)
+
+// function e(){setTimeout(() => {
+
+//     console.log("Pay");
+// }, 3000);
+//     }
+//     e();
+//     }
+//     d();
+//     }
+//     c();
+//     }
+//     b();
+// }
+
+// a();
