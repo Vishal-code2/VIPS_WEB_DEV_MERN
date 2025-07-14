afed0d0ad414064f5947e7fb5d34a9f6f218ae5d
Vishal <professional96gd@gmail.com>
2025-07-14 12:22:18 +0530

    commit

---
+++ L5_js_start/script.js
@@ -0,0 +1,15 @@
+// a = null;
+// console.log(a+"      ()    ");
+
+// var sum=(a,b)=>{
+//     return a+b;
+// }
+// // console.log(sum(10,20));
+// let ans=sum(10,20);
+// console.log(ans);
+
+let arr=[10,20,30,40]
+console.log(arr)
+let newarr=arr
+newarr.push(50);
+console.log(newarr);
