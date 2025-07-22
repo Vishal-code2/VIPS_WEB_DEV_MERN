a2701ad978a56cf075f29d1b0c90798f47fd534d
Vishal <professional96gd@gmail.com>
2025-07-22 22:16:25 +0530

    commit

---
+++ L8_js_end/script.js
@@ -0,0 +1,50 @@
+// const btn=document.querySelector(".button")
+// btn.addEventListener("click",()=>{
+//     let parent=
+// })
+
+
+
+// const list=document.querySelector(".list")
+
+// list.addEventListener("click",()=>{
+    // console.log(list.children);
+    //     console.log(list.firstElementChild);
+    //     console.log(list.lastElementChild);
+    //     console.log(list.firstChild);
+    //     console.log(list.lastChild);
+    
+    
+    // list.removeAttribute("class ");       //attributes
+    
+//     const list=document.querySelector(".list")
+//     console.log(list.id);
+
+// const url=document.querySelector("a")
+// console.log(url.href);
+
+// const source=document.querySelector("img")
+// console.log(source.src);
+
+
+// console.log(list.nextElementSibling);
+// console.log(list.previousElementSibling);
+
+    // console.log(list.id);
+    // list.id +="list1"
+    // console.log(list.id)
+
+    // })
+    
+    
+    let box=document.querySelector(".box")
+    box.addEventListener("mouseover",()=>{
+        console.log("mouse agya");
+    
+    })
+    
+    box.addEventListener("mouseout",()=>{
+        console.log("mouse gya");
+    
+    })
+    
