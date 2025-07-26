00cca799bb1c7df7d04f6096691e57fe6988f77c
Vishal <professional96gd@gmail.com>
2025-07-26 11:16:51 +0530

    commit

---
+++ BACKEND/L8/app.js
@@ -0,0 +1,79 @@
+// console.log("hello world");
+
+// const sum=require("./math")
+
+// console.log(sum(20,40));
+
+// const fs=require("fs")
+// fs.readFile("math.js","utf8",(err ,data )=>{
+//     if(err){
+//         console.log(err);
+//         return 
+//     }
+//     else{
+//         console.log(data);
+//     }
+// })
+
+const fs=require("fs")                      //this line need every time for fs 
+
+// fs.writeFile("text.txt","hello text file ",(err)=>{
+//     if (err){
+//         console.log(err)
+//         return
+//     }
+//     console.log("file created successfully")
+// })
+
+
+// fs.appendFile("text.txt", "i am appending the file .",(err)=>{
+//     if (err){
+//         console.log(err)
+//         return
+//     }
+//     console.log("data append")
+// })
+
+
+
+// fs.unlink("text.txt",(err)=>{             //delete the file
+//     if(err){
+//         console.log(err);
+
+//     }
+//     else{
+//         console.log("file was deleted");
+
+//     }
+// })
+
+
+// fs.mkdir("sample",()=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("folder created ")
+//     }
+// })
+
+// fs.readdir("sample",(err,file)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log(file);
+
+//     }
+// })
+
+// fs.rmdir("sample",(err,file)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("folder remove");
+
+//     }
+// })
+
